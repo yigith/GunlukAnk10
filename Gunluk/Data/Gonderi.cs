e495ffa3628c2ecbfe9ad83f7fa3159a640a1da4
yigith <yigith1@gmail.com>
2023-01-09 17:44:40 +0300

    summernote

--- Gunluk/Data/Gonderi.cs
+++ Gunluk/Data/Gonderi.cs
@@ -6,10 +6,12 @@ namespace Gunluk.Data
     {
         public int Id { get; set; }
 
-        [Required]
+        [Display(Name = "Başlık")]
+        [Required(ErrorMessage = "{0} alanı zorunludur.")]
         [MaxLength(400)]
         public string Baslik { get; set; } = null!;
 
+        [Display(Name = "İçerik")]
         public string? Icerik { get; set; } = "";
         
         [MaxLength(255)]
@@ -19,6 +21,7 @@ namespace Gunluk.Data
 
         public DateTime DegistirilmeZamani { get; set; } = DateTime.Now;
 
+        [Display(Name = "Kategori")]
         public int KategoriId { get; set; }
 
         public Kategori Kategori { get; set; } = null!;
